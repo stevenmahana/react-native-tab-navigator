c2d861c556794dbfec9e539eb797280959b9b2ab
Steve <steven.mahana@crossdeck.us>
2017-02-26 15:17:10 -0800

    initial commit

--- TabNavigator.js
+++ TabNavigator.js
@@ -77,7 +77,7 @@ export default class TabNavigator extends React.Component {
 
       let { selected } = item.props;
       let scene =
-        <SceneContainer key={sceneKey} selected={selected}>
+        <SceneContainer key={sceneKey} selected={selected} >
           {item}
         </SceneContainer>;
 
@@ -85,7 +85,7 @@ export default class TabNavigator extends React.Component {
     });
 
     return (
-      <View {...props} style={[styles.container, style]}>
+      <View {...props} >
         {scenes}
         <TabBar style={tabBarStyle} shadowStyle={tabBarShadowStyle}>
           {React.Children.map(children, this._renderTab)}
