399098d2525adc1c9a9f4b99a56a10c036518fd6
Steve <steven.mahana@crossdeck.us>
2017-02-26 15:11:37 -0800

    initial commit

--- TabNavigator.js
+++ TabNavigator.js
@@ -85,7 +85,12 @@ export default class TabNavigator extends React.Component {
     });
 
     return (
+      <View {...props} style={[styles.container, style]}>
 
+        <TabBar style={tabBarStyle} shadowStyle={tabBarShadowStyle}>
+          {React.Children.map(children, this._renderTab)}
+        </TabBar>
+      </View>
     );
   }
 
